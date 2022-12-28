bc75159ef51b93b4f61d3d35b973426a490acddd
Nurbekabdyrashitovich <nurbekabdyrashitovich@gmail.com>
2022-12-28 23:06:50 +0300

    first commit

---
+++ javaScript.js
@@ -0,0 +1,18 @@
+let city =document.querySelectorAll('.btn_b')
+
+function qwe (){
+    let forec =document.querySelector('.forecast');
+    forec.classList.toggle('active');
+   console.log('qwerty')
+}
+city[0].addEventListener('click',qwe)
+
+
+let city_1 =document.querySelectorAll('.btn')
+
+function qwe_1 (){
+    let forec_1 =document.querySelector('.forecast_1');
+    forec_1.classList.toggle('active');
+   console.log('qwerty')
+}
+city_1[0].addEventListener('click',qwe_1)
